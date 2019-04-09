1b336c517060c42bb0f7777be7f9512220b29052
Yeasir Arafat <yeasirarafat321@gmail.com>
2019-04-10 01:17:51 +0600

    Factory and seeding

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -1,9 +1,12 @@
 <?php
 
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(App\Model\Category::class, function (Faker $faker) {
+    $word = $faker->word;
     return [
-        //
+        'name' => $word,
+        'slug' => Str::slug($word)
     ];
 });
--- database/factories/ReplyFactory.php
+++ database/factories/ReplyFactory.php
@@ -1,9 +1,16 @@
 <?php
 
+use App\Model\Question;
 use Faker\Generator as Faker;
 
 $factory->define(App\Model\Reply::class, function (Faker $faker) {
     return [
-        //
+        'body' => $faker->text(),
+        'question_id' => function(){
+        return Question::all()->random();
+        },
+        'user_id' => function(){
+            return \App\User::all()->random();
+        }
     ];
 });
