6c6eaa250d42a740dabf4caa7aba19b994652c0b
Yeasir Arafat <yeasirarafat321@gmail.com>
2019-04-10 02:38:40 +0600

    Crud for Question

--- app/Model/Question.php
+++ app/Model/Question.php
@@ -7,6 +7,13 @@ use Illuminate\Database\Eloquent\Model;
 
 class Question extends Model
 {
+    protected $fillable = ['title','slug','body','category_id','user_id'];
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
+    }
+
     public function user(){
         return $this->belongsTo(User::class);
     }
@@ -17,5 +24,9 @@ class Question extends Model
 
     public function category(){
         return $this->belongsTo(Category::class);
+    }
+
+    public function getPathAttribute(){
+        return asset("api/question/$this->slug");
     }
 }
