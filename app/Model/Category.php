5ebe98c7902a304f4eb69bd53efabb1eba2040cf
Yeasir Arafat <yeasirarafat321@gmail.com>
2019-04-11 00:30:38 +0600

    Crud for category

--- app/Model/Category.php
+++ app/Model/Category.php
@@ -6,7 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Category extends Model
 {
+    protected $fillable = ['name','slug'];
+
     public function questions(){
         return $this->hasMany(Question::class);
+    }
+
+    public function getRouteKeyName()
+    {
+        return 'slug';
     }
 }
